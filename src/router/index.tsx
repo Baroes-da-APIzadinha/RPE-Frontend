fe4582eef6c0ad868261ae257801e9ccf56b4bbf
deadcube04 <gabrielmca2004@gmail.com>
2025-06-18 16:37:51 -0300

    chore: adicionando uma nova rota e 2 novas cores

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { Home } from '../pages/home'
-
+import  ColaboradorHome  from '../pages/colaborador/home/index.tsx'
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="colaborador/home" element={<ColaboradorHome />} />
       </Routes>
     </BrowserRouter>
   )
